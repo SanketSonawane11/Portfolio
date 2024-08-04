6acbb03bf321d347570394383f3fc0c9cedd6f24
SanketSonawane11 <sanketsonawane424@gmail.com>
2024-08-04 12:33:16 +0530

    Changed title

--- src/app/Components/Hero/Hero.tsx
+++ src/app/Components/Hero/Hero.tsx
@@ -26,7 +26,7 @@ function Hero() {
                 specializing in React and Next.js.
               </div>
             </HeroHighlight>
-            <button className="p-[3px] relative">
+            <button className="p-[3px] relative active:">
               <div className="absolute inset-0 bg-gradient-to-r from-indigo-500 to-purple-500 rounded-lg" />
               <div className="px-8 py-2 text-sm md:text-lg bg-black rounded-[6px]  relative group transition duration-200 text-white hover:bg-transparent">
                 See my work
