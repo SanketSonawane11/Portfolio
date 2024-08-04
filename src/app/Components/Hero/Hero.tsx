61aa4ffbdec860f6a1b3238cbd35bce800da2b47
SanketSonawane11 <sanketsonawane424@gmail.com>
2024-08-04 13:02:22 +0530

    Finalised hero animation

--- src/app/Components/Hero/Hero.tsx
+++ src/app/Components/Hero/Hero.tsx
@@ -1,30 +1,71 @@
-import React from "react";
+"use client";
+
+import React, { useEffect } from "react";
 import { Spotlight } from "../ui/Spotlight";
 import { GridBackground } from "../ui/Grid-background";
 import { HeroHighlight, Highlight } from "../ui/Hero-highlight";
 import HeroFade from "../ui/gsapFade";
+import { TextGenerateEffect } from "../ui/Text-generate-effect";
+import gsap from "gsap";
 
 function Hero() {
+  const word = "Crafting Engaging Web Experiences with Expertise in Tech";
+
+  const HeroTextFade = ({
+    className,
+    children,
+  }: {
+    className: string;
+    children: any;
+  }) => {
+    useEffect(() => {
+      const elements = document.querySelectorAll(".letter");
+      gsap.fromTo(
+        elements,
+        { opacity: 0 },
+        { opacity: 1, duration: 1, stagger: 0.05 }
+      );
+    }, []);
+
+    return <div className={className}>{children}</div>;
+  };
+
+  const splitText = (text: string) =>
+    text.split(" ").map((letter, index) => (
+      <span key={index} className="letter">
+        {letter}{" "}
+      </span>
+    ));
+
   return (
     <div>
       <Spotlight className="top-[-10%] left-[15%]" fill="blue" />
       <Spotlight className="top-[-30%] left-[-10%]" fill="white" />
       <GridBackground className="flex flex-col w-[100%] items-center justify-center">
-        <div className="text-center w-[32rem] md:min-w-[50rem] lg:min-w-[85rem] flex-col flex items-center gap-8 justify-center">
+        <div className="text-center w-[32rem] md:min-w-[50rem] lg:min-w-[85rem] flex-col flex items-center gap-5 justify-center">
           <HeroFade>
             <HeroHighlight className=" text-white/80 text-4xl md:text-5xl lg:text-6xl">
-              <h1 className="text-lg mb-5 md:mb-10 md:text-xl font-light text-white/60">
-                Elevating Web Development with Precision and Style
-              </h1>
-              Crafting{" "}
-              <Highlight className="text-black dark:text-white">
-                Engaging Web Experiences
-              </Highlight>{" "}
-              with with Expertise in Tech
-              <div className="text-xl mt-8 md:mt-5 text-white/70">
-                Hi! I’m Sanket, a <Highlight>Full-Stack Developer</Highlight>{" "}
-                specializing in React and Next.js.
-              </div>
+              <HeroTextFade className="text-white/80 text-4xl md:text-5xl lg:text-6xl">
+                <h1 className="text-lg mb-5 md:mb-10 md:text-xl font-light text-white/60">
+                  {splitText(
+                    "Elevating Web Development with Precision and Style"
+                  )}
+                </h1>
+                <p>
+                  {splitText("Crafting ")}
+                  <Highlight className="text-black dark:text-white">
+                    {splitText("Engaging Web Experiences")}
+                  </Highlight>{" "}
+                  {splitText(" with Expertise in Tech")}
+                </p>
+                <div className="text-xl mt-8 md:mt-5 text-white/70">
+                  Hi! I’m Sanket, a{" "}
+                  <Highlight className="text-black dark:text-white">
+                    {splitText("Full-Stack Developer")}
+                  </Highlight>{" "}
+                  specializing in React and Next.js.
+                </div>
+              </HeroTextFade>
             </HeroHighlight>
             <button className="p-[3px] relative active:">
               <div className="absolute inset-0 bg-gradient-to-r from-indigo-500 to-purple-500 rounded-lg" />
