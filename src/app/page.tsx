8fe9dbed69c645da61ef572fa647d3ca6646a05b
SanketSonawane11 <sanketsonawane424@gmail.com>
2024-08-31 20:28:21 +0530

    Fixed Dark mode UI

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,12 +4,10 @@ import About from "./Components/AboutGrid/About";
 import { FloatingNav } from "./Components/ui/Navbar";
 import { navItems } from "./data/navLinks";
 import Projects from "./Components/Projects/Projects";
-import Greeting from "./Components/Greeting";
 
 function page() {
   return (
     <div className="overflow-hidden bg-black w-[100vw] scroll-smooth">
-      {/* <Greeting /> */}
       <FloatingNav navItems={navItems} />
       <Hero />
       <About />
